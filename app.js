a912e9b355e809355c9b10f8e5dc2d508c2f06a4
DevrathIyer <idevrath@gmail.com>
2018-06-22 12:35:57 -0700

    changed bot to add mulitple dialogs

--- app.js
+++ app.js
@@ -19,47 +19,57 @@ server.post('/api/messages', connector.listen());
 
 var instructions = 'Welcome to the Bot to showcase the DirectLine API. Send \'Show me a hero card\' or \'Send me a BotFramework image\' to see how the DirectLine client supports custom channel data. Any other message will be echoed.';
 
-// Bot Storage: Here we register the state storage for your bot. 
+// Bot Storage: Here we register the state storage for your bot.    
 // Default store: volatile in-memory store - Only for prototyping!
 // We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
 // For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
 var inMemoryStorage = new builder.MemoryBotStorage();
 
-var bot = new builder.UniversalBot(connector, function (session) {
+var bot = new builder.UniversalBot(connector, [
+    // this section becomes the root dialog
+    // If a conversation hasn't been started, and the message
+    // sent by the user doesn't match a pattern, the
+    // conversation will start here
+    (session, args, next) => {
+        session.send(`Hi there! I'm a sample bot showing how multiple dialogs work.`);
+        session.send(`Let's start the first dialog, which will ask you your name.`);
 
-    var reply = new builder.Message()
-        .address(session.message.address);
+        // Launch the getName dialog using beginDialog
+        // When beginDialog completes, control will be passed
+        // to the next function in the waterfall
+        session.beginDialog('getName');
+    },
+    (session, results, next) => {
+        // executed when getName dialog completes
+        // results parameter contains the object passed into endDialogWithResults
 
-    var text = session.message.text.toLocaleLowerCase();
+        // check for a response
+        if (results.response) {
+            const name = session.privateConversationData.name = results.response;
 
-    console.log('[' + session.message.address.conversation.id + '] Message received: ' + text);
+            // When calling another dialog, you can pass arguments in the second parameter
+            session.beginDialog('getAge', { name: name });
+        } else {
+            // no valid response received - End the conversation
+            session.endConversation(`Sorry, I didn't understand the response. Let's start over.`);
+        }
+    },
+    (session, results, next) => {
+        // executed when getAge dialog completes
+        // results parameter contains the object passed into endDialogWithResults
 
-    switch (text) {
-        case 'show me a hero card':
-            reply.text('Sample message with a HeroCard attachment')
-                .addAttachment(new builder.HeroCard(session)
-                    .title('Sample Hero Card')
-                    .text('Displayed in the DirectLine client'));
-            break;
+        // check for a response
+        if (results.response) {
+            const age = session.privateConversationData.age = results.response;
+            const name = session.privateConversationData.name;
 
-        case 'send me a botframework image':
-            reply.text('Sample message with an Image attachment')
-                .addAttachment({
-                    contentUrl: 'https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png',
-                    contentType: 'image/png',
-                    name: 'BotFrameworkOverview.png'
-                });
-
-            break;
-
-        default:
-            reply.text('You said \'' + session.message.text + '\'');
-            break;
-    }
-
-    session.send(reply);
-
-}).set('storage', inMemoryStorage); // Register in memory storage
+            session.endConversation(`Hello ${name}. You are ${age}`);
+        } else {
+            // no valid response received - End the conversation
+            session.endConversation(`Sorry, I didn't understand the response. Let's start over.`);
+        }
+    },
+]).set('storage', inMemoryStorage); // Register in memory storage
 
 
 bot.on('conversationUpdate', function (activity) {
@@ -75,3 +85,82 @@ bot.on('conversationUpdate', function (activity) {
         });
     }
 });
+bot.dialog('getName', [
+    (session, args, next) => {
+        // store reprompt flag
+        if(args) {
+            session.dialogData.isReprompt = args.isReprompt;
+        }
+
+        // prompt user
+        builder.Prompts.text(session, 'What is your name?');
+    },
+    (session, results, next) => {
+        const name = results.response;
+
+        if (!name || name.trim().length < 3) {
+            // Bad response. Logic for single re-prompt
+            if (session.dialogData.isReprompt) {
+                // Re-prompt ocurred
+                // Send back empty string
+                session.endDialogWithResult({ response: '' });
+            } else {
+                // Set the flag
+                session.send('Sorry, name must be at least 3 characters.');
+
+                // Call replaceDialog to start the dialog over
+                // This will replace the active dialog on the stack
+                // Send a flag to ensure we only reprompt once
+                session.replaceDialog('getName', { isReprompt: true });
+            }
+        } else {
+            // Valid name received
+            // Return control to calling dialog
+            // Pass the name in the response property of results
+            session.endDialogWithResult({ response: name.trim() });
+        }
+    }
+]);
+
+bot.dialog('getAge', [
+    (session, args, next) => {
+        let name = session.dialogData.name = 'User';
+
+        if (args) {
+            // store reprompt flag
+            session.dialogData.isReprompt = args.isReprompt;
+
+            // retrieve name
+            name = session.dialogData.name = args.name;
+        }
+
+        // prompt user
+        builder.Prompts.number(session, `How old are you, ${name}?`);
+    },
+    (session, results, next) => {
+        const age = results.response;
+
+        // Basic validation - did we get a response?
+        if (!age || age < 13 || age > 90) {
+            // Bad response. Logic for single re-prompt
+            if (session.dialogData.isReprompt) {
+                // Re-prompt ocurred
+                // Send back empty string
+                session.endDialogWithResult({ response: '' });
+            } else {
+                // Set the flag
+                session.dialogData.didReprompt = true;
+                session.send(`Sorry, that doesn't look right.`);
+                // Call replaceDialog to start the dialog over
+                // This will replace the active dialog on the stack
+                session.replaceDialog('getAge', 
+                    { name: session.dialogData.name, isReprompt: true });
+            }
+        } else {
+            // Valid age received
+            // Return control to calling dialog
+            // Pass the age in the response property of results
+            session.endDialogWithResult({ response: age });
+        }
+    }
+]);
