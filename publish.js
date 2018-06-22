fcd5dd1093f261680f1ed5343d01524c42cad700
DevrathIyer <idevrath@gmail.com>
2018-06-22 12:01:51 -0700

    inital commit

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../tasqbot.zip');
+var kuduApi = 'https://tasqbot.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$tasqbot';
+var password = 'sxCw0mlQ9rfmqfl3dt7fRjjF0X5empZ1W3TX7CrGLmj4iZQpaak97cM9e2as';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('tasqbot publish');
+  } else {
+    console.error('failed to publish tasqbot', err);
+  }
+});
